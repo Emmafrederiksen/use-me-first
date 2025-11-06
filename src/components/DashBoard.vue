4810cce4f5b11fd4dbc0301d76275cedfe183e9c
sarahnymann <saj004@edu.zealand.dk>
2025-11-06 18:52:42 +0100

    added cards fro the recipes on dashboard, added img and styling to recipe cards

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -8,12 +8,61 @@
     </div>
 
     <AddNewCard />
+
+    <div class="mx-4 mt-5">
+      <div class="d-flex justify-content-between align-items-center mb-2">
+        <h5 class="recipes-text">Opskriftsforlag</h5>
+        <a href="#" class="see-all-text">Se alle</a>
+      </div>
+
+      <div class="card recipe-card mb-3">
+        <img v-bind:src="Rugbroedschips" class="card-img" alt="Rugbrødschips">
+        <div class="card-img-overlay d-flex flex-column justify-content-end">
+          <div class="icon-top d-flex justify-content-end">
+            <i class="bi bi-arrow-right-circle fs-1"></i>
+          </div>
+          <h5 class="card-title text-white">Rugbrødschips</h5>
+        </div>
+      </div>
+
+      <div class="row g-3">
+        <div class="col-6">
+          <div class="card recipe-card">
+            <img v-bind:src="Pandekager" class="card-img" alt="Pandekager">
+            <div class="card-img-overlay d-flex flex-column justify-content-end">
+            <div class="icon-top d-flex justify-content-end">
+              <i class="bi bi-arrow-right-circle fs-1"></i>
+            </div>
+            <h5 class="card-title text-white">Pandekager</h5>
+        </div>
+      </div>
+    </div>
+     <div class="col-6">
+      <div class="card recipe-card">
+        <img v-bind:src="Kylling" class="card-img" alt="Kylling i kokosmælk">
+        <div class="card-img-overlay d-flex flex-column justify-content-end">
+          <div class="icon-top d-flex justify-content-end">
+            <i class="bi bi-arrow-right-circle fs-1"></i>
+          </div>
+          <h5 class="card-title text-white">Kylling i kokosmælk</h5>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <br><br>
+  <br><br>
+
 </template>
 
 <script>
 
 import HeaderCard from './HeaderCard.vue';
 import AddNewCard from './AddNewCard.vue';
+import Rugbroedschips from '@/assets/rugbroedschips.jpg';
+import Pandekager from '@/assets/pandekager.jpg';
+import Kylling from '@/assets/kylling-ret.jpg';
 
 export default {
 
@@ -30,7 +79,10 @@ export default {
     
     return {
       showAlert: !dismissed, // hvis dismissed er true → skjul
-      itemCount: 3
+      itemCount: 3,
+      Rugbroedschips,
+      Pandekager,
+      Kylling,
     }
   },
 
@@ -52,6 +104,47 @@ export default {
 .alert {
   background: #ffffff;
   color: #2c2c2c;
+}
+
+.recipe-card {
+  position: relative;
+  overflow: hidden;
+  border-radius: 12px;
+  box-shadow: 0 10px 14px rgba(0,0,0,0.14);
+}
+
+.recipe-card .card-img {
+  height: 140px;
+  object-fit: cover;
+  filter: brightness(70%);
+}
+
+.recipe-card .card-img-overlay {
+  background: rgba(0,0,0,0.1);
+  color: white;
+}
+
+.icon-top {
+  position: absolute;
+  top: 10px;
+  right: 15px;
+}
+
+.card-title {
+  font-weight: 500;
+  font-size: 1.1rem;
+  margin-bottom: 0px;
+}
+
+.recipes-text {
+  font-weight: 600;
+}
+
+.see-all-text {
+  font-size: 0.9rem;
+  font-weight: 600;
+  color: #F27405;
+  text-decoration: underline;
 }
 
 </style>
