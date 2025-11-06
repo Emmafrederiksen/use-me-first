cffab4ba2ad22593f9d82373c87a93b437caf75b
sarahnymann <saj004@edu.zealand.dk>
2025-11-06 14:20:30 +0100

    added v-model an data binding to input element

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -1,15 +1,15 @@
 <template>
     <HeaderCard />
 
-    <form class="mx-4">
+    <form @submit.prevent="submitForm" class="mx-4">
         <div class="mb-4 mt-5">
             <label for="name" class="form-label bold-label">Varenavn *</label>
-            <input type="text" class="form-control" id="name" placeholder="Indtast navn..." required>
+            <input type="text" class="form-control" id="name" placeholder="Indtast navn..." v-model="Name">
         </div>
 
         <div class="mb-4">
             <label for="location" class="form-label bold-label">Placering</label>
-            <select id="location" class="form-select" aria-label="Vælg placering">
+            <select id="location" class="form-select" aria-label="Vælg placering" v-model="Location">
                 <option selected>Vælg placering</option>
                 <option value="1">Køleskab</option>
                 <option value="2">Fryser</option>
@@ -19,17 +19,17 @@
 
         <div class="mb-4">
             <label for="date" class="form-label bold-label">Udløbsdato *</label>
-            <input type="date" class="form-control" id="date" required>
+            <input type="date" class="form-control" id="date" v-model="Date">
         </div>
 
         <div class="d-flex align-items-center gap-3">
             <div class="mb-4" style="width: 30%;">
                 <label for="amount" class="form-label bold-label">Mængde</label>
-                <input type="number" class="form-control" id="amount" placeholder="1">
+                <input type="number" class="form-control" id="amount" placeholder="1" v-model="Amount">
             </div>
 
             <div class="mb-4" style="width: 70%;">
-                <select id="value" class="form-select" aria-label="Vælg enhed" style="margin-top: 32px;">
+                <select id="value" class="form-select" aria-label="Vælg enhed" style="margin-top: 32px;" v-model="Unit">
                     <option selected>Enhed</option>
                     <option value="1">Gram</option>
                     <option value="2">Bakker</option>
@@ -40,10 +40,10 @@
             </div>
         </div>
 
-        <router-link to="/indtast" class="add-btn mt-3">
+        <button to="/indtast" class="add-btn mt-3">
             <i class="bi bi-check2-circle me-2"></i>
             Gem vare
-        </router-link>
+        </button>
     </form>
 </template>
 
@@ -54,10 +54,44 @@ import HeaderCard from './HeaderCard.vue';
 export default {
 
     name: 'AddItem',
+
+    data() {
+        return {
+            Name: '',
+            Location: '',
+            Date: '',
+            Amount: null,
+            Unit: '',
+        }
+    },
     
     components: {
         HeaderCard,
     },
+    methods: {
+        submitForm() {
+
+            if(!this.Name || !this.Date) {
+                alert('Udfyld venligst alle påkrævede felter markeret med *');
+                return;
+            }
+
+            console.log('Name:' + this.Name);
+            this.Name = '';
+
+            console.log('Location:' + this.Location);
+            this.Location = '';
+
+            console.log('Date:' + this.Date);
+            this.Date = '';
+
+            console.log('Amount:' + this.Amount);
+            this.Amount = null;
+
+            console.log('Unit:' + this.Unit);
+            this.Unit = '';
+        }
+    }
 
 
 }
