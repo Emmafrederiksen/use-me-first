0eaf5618849a5e7de3c629a8a03aa8872030baa7
emmao <emf002@edu.zealand.dk>
2025-11-12 10:33:47 +0100

    Updated subtitle for AddItem

--- src/main.js
+++ src/main.js
@@ -38,7 +38,7 @@ const router = createRouter({
             component: AddItem,
             meta: {
                 title: 'Tilføj varer',
-                subtitle: 'Udfyld oplysningerne nedenfor for at tilføje en ny madvare.',
+                subtitle: 'Udfyld oplysningerne for din vare. Så hjælper vi dig med at holde styr på dit køleskab.',
             }
         },
 
