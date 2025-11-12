e9f8d609c8de4f4f5f882ac9e11e6b1704084ef1
emmao <emf002@edu.zealand.dk>
2025-11-12 11:01:25 +0100

    Updated width in AddItem.vue for unit and amount

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -22,18 +22,19 @@
         </div>
 
         <div class="d-flex align-items-center gap-3">
-            <div class="mb-4" style="width: 30%;">
+            <div class="mb-4" style="width: 40%;">
                 <label for="amount" class="form-label bold-label">Mængde</label>
-                <input type="number" class="form-control" id="amount" placeholder="1" v-model="Amount">
+                <input type="number" class="form-control" id="amount" placeholder="Indtast antal" v-model="Amount">
             </div>
 
-            <div class="mb-4" style="width: 70%;">
+            <div class="mb-4" style="width: 60%;">
                 <select id="value" class="form-select" aria-label="Vælg enhed" style="margin-top: 32px;" v-model="Unit">
                     <option value="1">Gram</option>
                     <option value="2">Bakke(r)</option>
                     <option value="3">Stk.</option>
                     <option value="4">Kilo</option>
                     <option value="5">Liter</option>
+                    <option value="6">Pakke(r)</option>
                 </select>
             </div>
         </div>
